0f369e11c63f556a591cac2f5b8faf1303332278
Jeremy Clermont <jer.clermont@gmail.com>
2026-04-28 23:50:13 -0400

    Modify create_file function to accept content parameter

--- crates/skc-util/src/lib.rs
+++ crates/skc-util/src/lib.rs
@@ -2,4 +2,4 @@ async fn create_directory(directory_path: &Path) {
     ::std::fs::create_dir_all(directory_path)
 }
 
-async fn create_file(file_path: &Path) {}
+async fn create_file(file_path: &Path, file_content: &[u8]) {}
