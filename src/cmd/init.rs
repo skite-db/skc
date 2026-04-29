cc14346e0f1458707f5f4d96289dfe4bd2af1451
Jeremy Clermont <jer.clermont@gmail.com>
2026-04-28 23:19:05 -0400

    Change return type of run_command_init and add comments
    Updated run_command_init to return skc_error::Result instead of io::Result. Added a comment outlining the initialization steps.

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -1,9 +1,16 @@
 use std::path::{Path, PathBuf};
 
-use tokio::io;
+/*
+1. vérifier si il n'existe deja pas si non Créer un dossier.skt/
+2. ajouté les fichiers nécessaires 
+*/
 
 
+pub const SKT_DIR = ".skt";
 
-pub async fn run_command_init(path: PathBuf) -> io::Result<()> {
+pub async fn run_command_init(path: PathBuf) -> skc_error::Result<()> {
+    
+    
+    
     Ok(())
 }
